df96c57761d839a586406afc0bc84ee5e30a1f93
LENOVO USER <talyagross@gmail.com>
2022-12-05 11:11:31 +0200

    new version

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-import logging
-logging.debug("hello")
+import pickle
+
+with open("database", "rb") as file:
+    #logging.debug("opened file")
+    data_dict = pickle.load(file)
+    print(data_dict)
